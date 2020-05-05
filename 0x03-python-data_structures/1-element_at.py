8913fbe1d8ef11e91517b4b08568239c8c258cae
Blazeker+ <BlackDiseasex@gmail.com>
2020-05-05 17:15:12 +0000

    Fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= 0:
-        return(my_list[idx])
-    return()
+    if idx >= list_len or idx < 0:
+        return
+    return (my_list[idx])
