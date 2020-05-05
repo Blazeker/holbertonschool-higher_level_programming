21fc772282f3cfc5ea7269dc13f385acf89ab27f
Blazeker+ <BlackDiseasex@gmail.com>
2020-05-05 17:19:46 +0000

    Fixed2

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-     if type(my_list) is list:
+    if type(my_list) is list:
         new_l = my_list[0:]
         new_l.reverse()
         for i in range(len(new_l)):
