1b035fa2f51b7b6db0cd75f6063d85b55ac4ede6
Blazeker+ <BlackDiseasex@gmail.com>
2020-05-07 22:17:43 +0000

    Fixed

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     res = 0.0
-    s_list = list(weight[0] * weight[1] for t in my_list)
+    s_list = list(weight[0] * weight[1] for weight in my_list)
     w_list = list(weight[1] for weight in my_list)
     res = sum(s_list) / sum(w_list)
     return res
