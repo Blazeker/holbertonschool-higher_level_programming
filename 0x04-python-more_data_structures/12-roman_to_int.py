00734c37abd40ec2ff6a48125e44434f9c45c8cc
Blazeker+ <BlackDiseasex@gmail.com>
2020-05-07 22:14:21 +0000

    Fixed and advanced

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,12 +6,14 @@ def roman_to_int(roman_string):
         return 0
     if not roman_string.isupper():
         return 0
-    rom_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    rom_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5,
+                'I': 1}
     cont = 0
+    ler = len(roman_string)
     for i in range(len(roman_string)):
         j = roman_string[i]
         if j in rom_dict:
-            if i < len(roman_string) - 1 and rom_dict[j] < rom_dict[roman_string[i + 1]]:
+            if i < ler - 1 and rom_dict[j] < rom_dict[roman_string[i + 1]]:
                 cont -= rom_dict[j]
             else:
                 cont += rom_dict[j]
