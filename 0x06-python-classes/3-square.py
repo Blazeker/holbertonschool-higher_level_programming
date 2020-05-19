6f9681242023c636a9d95c2dceaf3f023b60efa0
Blazeker <blackdiseasex@gmail.com>
2020-05-19 21:13:45 +0000

    Comments add

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Module of Square"""
+
+
 class Square:
+    """ An class called Square """
+
     def __init__(self, size=0):
+        """
+        Initialize the data
+        And validates if the number is an int
+        and > 0
+        Size: The size of the square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,5 +20,5 @@ class Square:
             self.__size = size
 
     def area(self):
-
+        """ Return the area of the square """
         return self.__size ** 2
