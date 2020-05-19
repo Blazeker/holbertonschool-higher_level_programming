16ae6cf3121e4ff6b51348b7177aeefdc695f965
Blazeker <blackdiseasex@gmail.com>
2020-05-19 21:18:54 +0000

    Comments add

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
+"""Module of Square"""
+
+
 class Square:
+    """ An class called Square """
+
     def __init__(self, size=0):
+        """
+        Initialize the data
+        And validates if the number is an int
+        and > 0
+        Size: The size of the square
+        """
         if self.__validate(size):
             self.__size = size
 
     @property
     def size(self):
+        """ Gets the value """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Sets the value on size and validate a valid number """
         if self.__validate(value):
             self.__size = value
 
     def __validate(self, size):
+        """ Validates if the number is an int and > 0 """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -23,9 +37,11 @@ class Square:
         return False
 
     def area(self):
+        """ Return the area of the square """
         return self.__size ** 2
 
     def my_print(self):
+        """ Print all the square with # """
             for i in range(0, self.__size):
                 j = 0
                 for j in range(0, self.__size):
