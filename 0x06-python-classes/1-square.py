b949f17e73c78085346b81723327d38a8e5283f1
Blazeker <blackdiseasex@gmail.com>
2020-05-19 21:07:42 +0000

    Comments add

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Module of Square"""
+
+
 class Square:
+    """ An class called Square """
+
     def __init__(self, size):
+        """
+        Initialize the data
+        size: The size of the square
+        """
         self.__size = size
