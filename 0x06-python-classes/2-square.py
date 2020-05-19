e05904a7705638bc7fd20dbfe1ae382b06db0bf6
Blazeker <blackdiseasex@gmail.com>
2020-05-19 21:09:40 +0000

    Comments add

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Module of Square"""
+
+
 class Square:
+    """ An class called Square """
+
     def __init__(self, size=0):
+        """
+        Initialize the data
+        And validates if the number is an int
+        and > 0
+        Size: The size of the square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
