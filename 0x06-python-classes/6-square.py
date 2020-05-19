f704fd5bb774fd1177279af23c07ca4a7b48e11c
Blazeker <blackdiseasex@gmail.com>
2020-05-19 21:42:40 +0000

    Comments add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,12 +23,13 @@ class Square:
 
     @position.setter
     def position(self, value):
+        message = "position must be a tuple of 2 positive integers"
         """ Sets the value on size and validate a valid position """
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        for items in value:
+            if type(items) != int or items < 0:
+                raise TypeError(message)
         self.__position = value
 
     @property
@@ -60,9 +61,9 @@ class Square:
         """ Print all the square with # """
         if self.__size == 0:
             print()
-            return
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+
+        for new in range(self.__position[1]):
+            print()
+
+        for row in range(self.__size):
+            print((' ' * self.__position[0]) + ('#' * self.__size))
