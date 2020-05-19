e69a151e271d7220191c97b09257a0c312a0fc52
Blazeker <blackdiseasex@gmail.com>
2020-05-19 22:38:20 +0000

    Comments add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,22 +10,22 @@ class Square:
         Size: The size of the square
         Position: The position of the square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
-
-    @property
-    def position(self):
-        return(self.__position)
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         return(self.__size)
 
+    @property
+    def position(self):
+        return(self.__position)
+
     @size.setter
     def size(self, value):
-        if type(size) != int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
