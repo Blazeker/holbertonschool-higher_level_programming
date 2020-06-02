cd13fdfa5e83b2381fcf7e730d8edb40663780c1
Blazeker <1687@holbertonschool.com>
2020-06-02 23:30:41 +0000

    Fixed

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,12 +8,9 @@ class Square(Rectangle):
     """ Square class """
 
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        return (self.__size * self.__size)
+        super().__init__(size, size)
 
     def __str__(self):
         return ("[Square] " + str(self.__size) + '/' + str(self.__size))
