bc5be4a41aaeefc4ea85103e77107443ce581e1c
Blazeker <1687@holbertonschool.com>
2020-06-02 23:27:35 +0000

    Fixed3.0aaaaaaaaaaaaa

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,19 +2,6 @@
 """ Module for geometry class """
 
 
-class BaseGeometry():
-    """ Geometry class with exceptions """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
-
-
 class Rectangle(BaseGeometry):
     """ Rectangle class """
 
