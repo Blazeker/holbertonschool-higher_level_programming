630aad23f14d2e87dfe5652e7d14c523d6bd6783
Blazeker <1687@holbertonschool.com>
2020-06-02 23:28:30 +0000

    Fixed3.0aaaaaaaaaaaaa

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,22 +2,7 @@
 """ Module for geometry class """
 
 
-class Rectangle(BaseGeometry):
-    """ Rectangle class """
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        string = "[Rectangle] "
-        string += str(self.__width) + '/' + str(self.__height)
-        return string
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
