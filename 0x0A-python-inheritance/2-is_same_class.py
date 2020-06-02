645dd52c97097b09d017b1eb5654e1cb2265a2e2
Blazeker <1687@holbertonschool.com>
2020-06-02 23:13:35 +0000

    Fixed

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" module for the same class """
+
+
 def is_same_class(obj, a_class):
+    """ returns the comparison between obj and a_class """
     return (type(obj) is a_class)
