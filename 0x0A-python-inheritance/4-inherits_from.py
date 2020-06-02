e29323ef3b4ffc9f58870ec099e2334120e82c66
Blazeker <1687@holbertonschool.com>
2020-06-02 23:18:43 +0000

    Fixed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+""" module for the inherit class """
 
 
 def inherits_from(obj, a_class):
+    """ returns the inherit types """
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
