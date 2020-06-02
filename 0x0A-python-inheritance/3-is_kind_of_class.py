8d65505b092e340dcd0a1ed4d3a7872d2faad918
Blazeker <1687@holbertonschool.com>
2020-06-02 23:17:28 +0000

    Fixed

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" module for the kind class """
+
+
 def is_kind_of_class(obj, a_class):
+    """ returns the comparison between obj and a_class """
     return (isinstance(obj, a_class) or issubclass(type(obj), a_class))
