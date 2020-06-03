cbf91e8d687d713892161b17ead2509833b98500
Blazeker <60364105+Blazeker@users.noreply.github.com>
2020-06-02 20:03:00 -0500

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,10 +5,7 @@
 def add_attribute(obj, name, value):
     """ Add attribute exercise """
 
-    type_z = (str, int, complex, bool, float, list, tuple, dict,
-              set, frozenset, type, object)
-    for type_s in type_z:
-        if type(obj) is type_s:
-            raise TypeError("can't add new attribute")
-
-    object.__setattr__(obj, name, value)
+    if hasattr(obj, "__dict__"):
+        object.__setattr__(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")    
