aaabf87e58937523a656b282cc0bcbf9c0cc6a19
Blazeker <1687@holbertonschool.com>
2020-06-09 23:48:24 +0000

    git errors

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,9 +8,12 @@ class Square(Rectangle):
     """ Square class """
 
     def __init__(self, size):
+
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
+
+    def area(self):
+        return (self.__size * self.__size)
 
     def __str__(self):
         return ("[Square] " + str(self.__size) + '/' + str(self.__size))
