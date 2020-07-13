fc6e5981e2cdfe8ab843eaaca84675ce0b3e76ee
Blazeker <1687@holbertonschool.com>
2020-07-13 17:18:38 +0000

    My sql introduction

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creates database and not fails if missing
+-- Creates database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- script that updates the score of Bob to 10 in the table second_table.
+-- Updates the score
+UPDATE `second_table` SET `score` WHERE `second_table`.`name` = "Bob";
