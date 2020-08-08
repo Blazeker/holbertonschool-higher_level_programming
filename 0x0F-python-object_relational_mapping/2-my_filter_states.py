e2bfd71f878ab032dd3e43ada8de8775e787ffa1
Blazeker <1687@holbertonschool.com>
2020-08-08 17:38:36 +0000

    Third exercise

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa:
+ Your script should take 3 arguments: mysql username, mysql password
+ and database name (no argument validation needed) """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    name = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+                WHERE name = '{}'
+                ORDER BY id""".format(name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
