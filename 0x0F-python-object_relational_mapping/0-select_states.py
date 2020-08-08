4f8be8f75eebcf657c3371a3486b94d25e2bff78
Blazeker <1687@holbertonschool.com>
2020-08-08 16:38:00 +0000

    Pep8fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Write a script that lists all states from the database hbtn_0e_0_usa:
-# Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+# Your script should take 3 arguments: mysql username, mysql password
+# and database name (no argument validation needed)
 import sys
 import MySQLdb
 
@@ -9,7 +10,8 @@ if __name__ == "__main__":
     if len(args) < 4:
         print("Usage: {} username password database_name".format(args[0]))
         exit(1)
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
